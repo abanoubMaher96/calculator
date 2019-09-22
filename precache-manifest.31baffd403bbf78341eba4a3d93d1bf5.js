00f034895f7b83def2bc324a717504c098ef79a2
unknown <abanoubmaher2015@gmail.com>
2019-09-22 21:52:20 +0200

    Updates

---
+++ precache-manifest.31baffd403bbf78341eba4a3d93d1bf5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682c6cc30e296af5011b1552180ffe11",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "bc3ce2865c4cb2089209",
+    "url": "/calculator/static/js/2.bf594555.chunk.js"
+  },
+  {
+    "revision": "2c0a9d1d7b786cb2c97c",
+    "url": "/calculator/static/js/main.79252802.chunk.js"
+  },
+  {
+    "revision": "25a2c020c3f89b568db5",
+    "url": "/calculator/static/js/runtime~main.ebcd6a31.js"
+  }
+]);
